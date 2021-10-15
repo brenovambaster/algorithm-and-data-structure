43e6cd8a04a01f3df59483eb2c0deacee6eca2c4
Breno Vambáster <brenovambaster5@gmail.com>
2021-10-15 11:34:50 -0300

    reorganizacao de aquivos

--- C++/learn/sem4/main.cpp
+++ C++/learn/sem4/main.cpp
@@ -11,10 +11,13 @@ int main()
     Aluno aluno2(40, "lucas");
     Aluno aluno3(39, "Maria");
     Lista lista1;
+
     lista1.insere(0,aluno1);
     lista1.insere(1,aluno2);
     lista1.insere(0,aluno3);
     lista1.imprime();
+
+    
 
     return 0;
 }
